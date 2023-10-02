866ef2970e5d0af7cf00170fdfc095e1ee170d8c
pilatdenis99 <108524025+pilatdenis99@users.noreply.github.com>
2023-10-02 01:45:17 -0700

    Delete controllers directory

--- controllers/coutriesController.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Country = require("../models/country.model");
-
-const getCountries = async (req, res) => {
-  const countries = await Country.find({});
-  res.status(200).json(countries);
-};
-
-module.exports = getCountries;
