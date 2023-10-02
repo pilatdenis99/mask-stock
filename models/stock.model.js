cdf6d449fd09570d3950cf0fbd6e161389299bac
pilatdenis99 <108524025+pilatdenis99@users.noreply.github.com>
2023-10-02 01:45:41 -0700

    Delete models directory

--- models/stock.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const stockSchema = new mongoose.Schema({
-  quantity: { type: Number, required: true },
-});
-
-module.exports = mongoose.model("Stock", stockSchema);
